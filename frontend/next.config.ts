089a089273cff6f8faf84d7b55bc4e64579def57
Ayroid <ayushsk0000@gmail.com>
2026-01-10 16:46:37 +0530

    update: middleware to proxy as per nextjs specification

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
 	images: {
@@ -24,6 +25,9 @@ const nextConfig: NextConfig = {
 				pathname: "/**",
 			},
 		],
+	},
+	turbopack: {
+		root: path.join(__dirname, ".."),
 	},
 };
 
